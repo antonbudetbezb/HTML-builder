977c51877d3e59b80373395cb64938d5184dca61
antonbudetbezb <nomoresatellite@gmail.com>
2022-11-07 19:13:18 +0300

    feat: implement 3rd task

---
+++ 03-files-in-folder/index.js
@@ -0,0 +1,17 @@
+const fs = require('fs');
+const path = require('path');
+
+fs.readdir(path.join(__dirname, 'secret-folder'), {withFileTypes: true}, (err, files) => {
+  if (err) throw new Error(err);
+  files.forEach(file => {
+    if (!file.isDirectory()) {
+      let filename =  file.name;
+      let ext = path.parse(filename).ext.slice(1);
+      let name = path.parse(filename).name;
+      let stats = fs.stat(path.join(__dirname, 'secret-folder', `${filename}`), (err, stats) => {
+        console.log(`${name}-${ext}-${Math.ceil(stats.size/1024)}`)
+      })
+    }
+  })
+})
+
